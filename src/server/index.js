ff7ed8b09d16ffb81883e144e9d67680b6ddffdc
Dan Schultz <slifty@gmail.com>
2019-05-10 14:31:36 -0400

    Create abstract crawl tooling
    Crawlers take in a URL and spit out a list of URLS that need to be
    scraped in more detail.  This commit sets up the object model for our
    various crawlers and creates an example implementation.
    
    This does not offer a good way to actually trigger crawls yet.
    
    AbstractCrawler is intended to be an abstract class (though abstract
    classes are not explicitly supported in javascript).  Specifically, it
    requires an extended class to implement the crawlHandler method.
    
    Issue #20

---
+++ src/server/index.js
@@ -0,0 +1,4 @@
+import HelloWorldCrawler from './workers/crawlers/HelloWorldCrawler'
+
+const crawler = new HelloWorldCrawler()
+crawler.run()
---
+++ src/server/workers/crawlers/HelloWorldCrawler.js
@@ -0,0 +1,14 @@
+import AbstractCrawler from './AbstractCrawler'
+
+class HelloWorldCrawler extends AbstractCrawler {
+  constructor() {
+    super('https://google.com')
+  }
+
+  crawlHandler = (responseString) => {
+    console.log(`Hello, I crawled ${responseString}`)
+    return []
+  }
+}
+
+export default HelloWorldCrawler
