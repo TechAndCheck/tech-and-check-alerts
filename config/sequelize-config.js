d19ea234c8f144eee5aea47e6b2a0a4c646174cc
Justin Reese <justin@justinreese.com>
2019-05-16 16:49:26 -0500

    Make ESLint happier about our Sequelize config
    Affects #17

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -1,17 +1,18 @@
 const dotenv = require('dotenv')
-dotenv.config();
+
+dotenv.config()
 
 module.exports = {
-  "development": {
-    "url": process.env.DATABASE_URL_DEVELOPMENT || "",
-    "dialect": "postgres"
+  development: {
+    url: process.env.DATABASE_URL_DEVELOPMENT || '',
+    dialect: 'postgres',
   },
-  "test": {
-    "url": process.env.DATABASE_URL_TEST || "",
-    "dialect": "postgres"
+  test: {
+    url: process.env.DATABASE_URL_TEST || '',
+    dialect: 'postgres',
   },
-  "production": {
-    "url": process.env.DATABASE_URL_PRODUCTION || "",
-    "dialect": "postgres"
-  }
+  production: {
+    url: process.env.DATABASE_URL_PRODUCTION || '',
+    dialect: 'postgres',
+  },
 }
