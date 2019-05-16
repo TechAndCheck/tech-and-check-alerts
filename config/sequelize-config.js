324c440da35bca1486ec387d4effbc96833efe39
Justin Reese <justin@justinreese.com>
2019-05-16 12:11:45 -0500

    Hotfix Sequelize’s references to database env vars
    Didn’t catch this when reviewing #38.
    
    Affects #17

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 module.exports = {
   "development": {
-    "url": process.env.DATABASE_URL_DEV || "",
+    "url": process.env.DATABASE_URL_DEVELOPMENT || "",
     "dialect": "postgres"
   },
   "test": {
@@ -11,7 +11,7 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "url": process.env.DATABASE_URL_PROD || "",
+    "url": process.env.DATABASE_URL_PRODUCTION || "",
     "dialect": "postgres"
   }
 }
