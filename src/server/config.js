496ab4d9ff55ffee2acfbbd47bad5da5f1d54ac0
Justin Reese <justin@justinreese.com>
2019-05-31 18:04:16 -0500

    Automatically config all process.env variables
    Instead of having to manually list any `.env`-set variables that we
    want added to our own `config` object (see more description in #49),
    just grab all of `process.env`. Now we don’t have yet another place to
    add a new environment variable.
    
    Affects #49

--- src/server/config.js
+++ src/server/config.js
@@ -3,11 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const {
-  REDIS_URL,
   ENABLED_QUEUE_DIRECTORIES,
 } = process.env
 
 export default {
-  REDIS_URL,
+  ...process.env,
   ENABLED_QUEUE_DIRECTORIES: ENABLED_QUEUE_DIRECTORIES.split(','),
 }
