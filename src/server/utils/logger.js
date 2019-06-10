665f0eb6003dd581146874cc2210a2f40fa17f27
Dan Schultz <slifty@gmail.com>
2019-06-10 13:40:13 -0400

    Create configured logger
    This makes a configured instance of a winston logger available for use
    in the rest of the code base.  It does not replace instances of
    console.log but the intent is that we can simply import this logger into
    a file and use logger.* to write to the appropriate log destination.
    
    All logs are written as json in a logs/* directory.  Errors are written
    to two files (errors.log and consolidated.log) while other levels of
    logging are only written to the consolidated log file.
    
    In non-production environments the logs are also written to console.
    
    Issue #34

---
+++ src/server/utils/logger.js
@@ -0,0 +1,47 @@
+import {
+  createLogger,
+  format,
+  transports,
+} from 'winston'
+
+import config from '../config'
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.errors({
+      stack: true,
+    }),
+    format.splat(),
+    format.json(),
+  ),
+  defaultMeta: { service: 'tech-and-check-alerts' },
+  transports: [
+    new transports.File({
+      filename: 'logs/error.log',
+      level: 'error',
+    }),
+    new transports.File({
+      filename: 'logs/combined.log',
+    }),
+  ],
+})
+
+if (config.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.combine(
+      format.timestamp({
+        format: 'HH:mm:ss',
+      }),
+      format.colorize(),
+      format.timestamp(),
+      format.align(),
+      format.printf(info => `${info.timestamp} (${info.level}): ${info.message}`),
+    ),
+  }))
+}
+
+export default logger
