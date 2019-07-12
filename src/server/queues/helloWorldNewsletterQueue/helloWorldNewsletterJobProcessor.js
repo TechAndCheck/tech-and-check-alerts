b5edca74d0bbf7ddf27a81daab1b1c38fe7ef593
Justin Reese <justin@justinreese.com>
2019-07-12 13:42:10 -0500

    Remove HelloWorldNewsletter queue
    It doesn’t really serve a purpose.
    
    Affects #23

--- src/server/queues/helloWorldNewsletterQueue/helloWorldNewsletterJobProcessor.js
+++
@@ -1,6 +0,0 @@
-import { HelloWorldNewsletter } from '../../newsletters'
-
-// We may customize behavior based on job parameters, so don't forget how.
-// But for now, don't complain that we aren't using `job`.
-// eslint-disable-next-line no-unused-vars
-export default job => (new HelloWorldNewsletter()).send()
--- src/server/queues/helloWorldNewsletterQueue/index.js
+++
@@ -1,9 +0,0 @@
-import HelloWorldNewsletterQueueFactory from './HelloWorldNewsletterQueueFactory'
-import HelloWorldNewsletterJobScheduler from './HelloWorldNewsletterJobScheduler'
-import helloWorldNewsletterJobProcessor from './helloWorldNewsletterJobProcessor'
-
-export default {
-  factory: new HelloWorldNewsletterQueueFactory(),
-  scheduler: new HelloWorldNewsletterJobScheduler(),
-  processor: helloWorldNewsletterJobProcessor,
-}
