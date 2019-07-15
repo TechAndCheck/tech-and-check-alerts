1c41b6765b6001eadd714146e74f2a16607dff12
Justin Reese <justin@justinreese.com>
2019-07-15 18:13:24 -0500

    Add comments to AbstractNewsletter methods
    Document the expected return values and types for abstract methods.
    
    Affects #23
    
    Co-authored-by: Daniel Schultz <slifty@gmail.com>

--- src/server/newsletters/AbstractNewsletter.js
+++ src/server/newsletters/AbstractNewsletter.js
@@ -1,26 +1,61 @@
 import { getFileContents } from '../utils'
 import { getHandlebarsTemplate } from '../utils/templates'
-import { getAddressForMailingList } from '../utils/mailer'
 import Mailer from '../workers/mailer'
 import logger from '../utils/logger'
+
+import { MAILING_LIST_ADDRESSES } from '../constants'
 
 class AbstractNewsletter {
   constructor() {
     this.mailer = new Mailer()
   }
 
+  /**
+   * Abstract method that provides the mailing list key name associated with the newsletter.
+   * However, don't return the key name as a string literal; return instead the value of the
+   * appropriate property from the MAILING_LISTS constant, e.g. `return MAILING_LISTS.DEVELOPERS`.
+   *
+   * OVERRIDE WHEN EXTENDING
+   *
+   * @return {string} The key name of the associated mailing list
+   */
   getMailingList = () => {
     throw new Error('You extended AbstractNewsletter but forgot to define getMailingList()')
   }
 
+  /**
+   * Abstract method that provides the absolute path to the newsletter's Handlebars template.
+   *
+   * OVERRIDE WHEN EXTENDING
+   *
+   * @return {string} The absolute path to the associated Handlebars template
+   */
   getPathToTemplate = () => {
     throw new Error('You extended AbstractNewsletter but forgot to define getPathToTemplate()')
   }
 
+  /**
+   * Abstract method that provides the email subject line for the newsletter.
+   *
+   * OVERRIDE WHEN EXTENDING
+   *
+   * @return {string} The newsletter's email subject line
+   */
   getSubject = () => {
     throw new Error('You extended AbstractNewsletter but forgot to define getSubject()')
   }
 
+  /**
+   * Abstract method that returns the data used to compose the newsletter body.
+   * The object returned will be passed to the templating function, and its
+   * properties made available to the template as local variables.
+   *
+   * This is an async function because it will often be querying the database.
+   *
+   * OVERRIDE WHEN EXTENDING
+   *
+   * @return {object} The object that will be passed to the templating function
+   */
   getBodyData = async () => {
     throw new Error('You extended AbstractNewsletter but forgot to define getBodyData()')
   }
