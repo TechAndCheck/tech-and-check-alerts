514db52584a4784a44efacb00979aaea577ab020
Justin Reese <justin@justinreese.com>
2019-08-19 12:18:21 -0500

    Fix typo in sandbox instructions
    This fixes a typo in d04415312f36e6f8f597732b79ffd40a8db65004.
    
    And just like that, the value of PR review reveals itself.

--- src/scripts/_sandbox.js
+++ src/scripts/_sandbox.js
@@ -3,4 +3,4 @@
 // To run this code, type `yarn sandbox` in your CLI.
 //
 // Do not commit changes to this file! In fact, you should locally set the `--assume-unchanged`
-// flag on it: `git update-index --assume-unchanged ./_sandbox.js`
+// flag on it: `git update-index --assume-unchanged ./src/scripts/_sandbox.js`
