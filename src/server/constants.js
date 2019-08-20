11d1bf740bddd12cdc13fdcfbced101ff75ec3bf
Justin Reese <justin@justinreese.com>
2019-08-20 17:12:34 -0500

    Add CNN show names to newsletters
    Now that we’re storing the show shortcode on each claim (86f9eef),
    let’s display the show name in the newsletter!
    
    This adds the constant that maps shortcodes to longnames and the
    Handlebars helper that performs the mapping. This returns empty if the
    claim didn’t come from the CNN scraper (or if we don't recognize the
    show name); that’s because not all scrapers will want their source
    shown in the newsletter. (E.g., the Twitter scraper will store the
    username as the source, but we won’t actually want that displayed.)
    
    I’m not wildly keen that we’re embedding markup in this Handlebars
    helper, but it’s kosher.
    
    The text version still generates the version of the source name with
    HTML markup; that’s not ideal, but it gets stripped out by our
    sanitizer anyway.
    
    Affects #149

--- src/server/constants.js
+++ src/server/constants.js
@@ -10,3 +10,34 @@ export const CLAIMBUSTER_THRESHHOLD = 0.5
 export const CLAIM_PLATFORM_NAMES = {
   CNN_TRANSCRIPT: 'CNN',
 }
+
+// Keys should be lower-case to match the value we pull from the URL and store in the database
+export const CNN_SHOW_NAMES = {
+  acd: 'Anderson Cooper 360 Degrees',
+  ampr: 'CNN\'s Amanpour',
+  ath: 'At This Hour',
+  cg: 'The Lead With Jake Tapper',
+  cnnt: 'CNN Tonight',
+  cnr: 'CNN Newsroom',
+  crn: 'CNN Right Now',
+  csr: 'CNN Special Reports',
+  ctw: 'Connect The World',
+  ebo: 'Erin Burnett Outfront',
+  es: 'Early Start',
+  fzgps: 'Fareed Zakaria GPS',
+  ip: 'Inside Politics',
+  nday: 'New Day',
+  ndaysat: 'New Day Saturday',
+  ndaysun: 'New Day Sunday',
+  nwsm: 'News Stream',
+  qmb: 'Quest Means Business',
+  rs: 'Reliable Sources',
+  se: 'Special Event',
+  sitroom: 'The Situation Room',
+  smer: 'Smerconish',
+  sn: 'CNN 10',
+  sotu: 'State of the Union',
+  vssg: 'Vital Signs With Dr. Sanjay Gupta',
+  wolf: 'Wolf',
+  wrn: 'World Right Now With Hala Gorani',
+}
