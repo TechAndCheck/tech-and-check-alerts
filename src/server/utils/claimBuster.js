0dc0577bcb0834cf0e92cb45f82e6ee5b7d2946b
Dan Schultz <slifty@gmail.com>
2019-09-01 21:02:22 -0400

    Clean claimbuster inputs
    Claimbuster does not handle newlines or forward slashes (even uri
    encoded ones).  This clean up claimbuster parameters so they don't
    break.
    
    Note that it may be that claimbuster v2 will remove the need for this
    cleanup.
    
    Issue #204

--- src/server/utils/claimBuster.js
+++ src/server/utils/claimBuster.js
@@ -1,7 +1,27 @@
-// Disabling because we intend to have more exports in the future.
-/* eslint-disable import/prefer-default-export */
-
+import {
+  runSequence,
+  squish,
+} from '.'
 import { CLAIMBUSTER_THRESHHOLD } from '../constants'
 
 export const filterWeakClaims = claimBusterResults => claimBusterResults
   .filter(result => result.score > CLAIMBUSTER_THRESHHOLD)
+
+const removeUnfriendlyCharacters = string => string.replace(/\//g, '')
+
+
+/**
+ * This cleans up statement text to match ClaimBuster API requirements.
+ *
+ * This method will change as the integration with ClaimBuster is updated.
+ *
+ * @param  {String} statementText The raw statement text to be passed to ClaimBuster
+ * @return {String}               The sanitized, ClaimBuster friendly statement text
+ */
+export const cleanTextForClaimBuster = statementText => runSequence(
+  [
+    removeUnfriendlyCharacters,
+    squish,
+  ],
+  statementText,
+)
