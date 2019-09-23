fcf2e7ba1749e2464687bfbe55f2bf3415eb4674
Justin Reese <justin@justinreese.com>
2019-09-23 06:18:42 -0500

    Wrap migration in a transaction
    Per feedback by @slifty, we should wrap this migration in a transaction.
    
    Affects #253
    
    Co-authored-by: Daniel Schultz <slifty@gmail.com>
    Signed-off-by: Justin Reese <justin@justinreese.com>

--- src/server/migrations/20190915180014-increase-claim-column-length.js
+++ src/server/migrations/20190915180014-increase-claim-column-length.js
@@ -1,39 +1,46 @@
 import { runPromiseSequence } from '../utils'
 
 module.exports = {
-  up: (queryInterface, Sequelize) => runPromiseSequence([
-    () => queryInterface.changeColumn(
-      'claims',
-      'source',
-      Sequelize.STRING(1024),
-    ),
-    () => queryInterface.changeColumn(
-      'claims',
-      'speaker_name',
-      Sequelize.STRING(1024),
-    ),
-    () => queryInterface.changeColumn(
-      'claims',
-      'speaker_affiliation',
-      Sequelize.STRING(1024),
-    ),
-  ]),
-
-  down: (queryInterface, Sequelize) => runPromiseSequence([
-    () => queryInterface.changeColumn(
-      'claims',
-      'source',
-      Sequelize.STRING,
-    ),
-    () => queryInterface.changeColumn(
-      'claims',
-      'speaker_name',
-      Sequelize.STRING,
-    ),
-    () => queryInterface.changeColumn(
-      'claims',
-      'speaker_affiliation',
-      Sequelize.STRING,
-    ),
-  ]),
+  up: (queryInterface, Sequelize) => queryInterface.sequelize
+    .transaction(t => runPromiseSequence([
+      () => queryInterface.changeColumn(
+        'claims',
+        'source',
+        Sequelize.STRING(1024),
+        { transaction: t },
+      ),
+      () => queryInterface.changeColumn(
+        'claims',
+        'speaker_name',
+        Sequelize.STRING(1024),
+        { transaction: t },
+      ),
+      () => queryInterface.changeColumn(
+        'claims',
+        'speaker_affiliation',
+        Sequelize.STRING(1024),
+        { transaction: t },
+      ),
+    ])),
+  down: (queryInterface, Sequelize) => queryInterface.sequelize
+    .transaction(t => runPromiseSequence([
+      () => queryInterface.changeColumn(
+        'claims',
+        'source',
+        Sequelize.STRING,
+        { transaction: t },
+      ),
+      () => queryInterface.changeColumn(
+        'claims',
+        'speaker_name',
+        Sequelize.STRING,
+        { transaction: t },
+      ),
+      () => queryInterface.changeColumn(
+        'claims',
+        'speaker_affiliation',
+        Sequelize.STRING,
+        { transaction: t },
+      ),
+    ])),
 }
