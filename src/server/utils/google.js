78ae58579fdaab450e916cc35131a44e2b296abf
Dan Schultz <slifty@gmail.com>
2020-01-10 14:52:45 -0500

    Add support for named columns in google scraper
    Google spreadsheets can now be scraped in a way that results in objects
    instead of arrays for each row, where the object attributes are mapped
    to the named columns of the spreadsheet.
    
    Issue #281

--- src/server/utils/google.js
+++ src/server/utils/google.js
@@ -2,10 +2,21 @@ import parse from 'csv-parse'
 
 export const getSpreadsheetCsvUrl = spreadsheetId => `https://docs.google.com/spreadsheets/d/${spreadsheetId}/export?format=csv`
 
-export const parseCsvString = async googleCsvString => new Promise((resolve, reject) => {
-  const output = []
-  parse(googleCsvString)
-    .on('data', record => output.push(record))
-    .on('error', error => reject(error))
-    .on('end', () => resolve(output))
-})
+/**
+ * Take a CSV string and return a parsed array.
+ *
+ * @param  {String}   googleCsvString The CSV string to be parsed.
+ * @param  {bool}     columnHeaders true if the first row should be parsed as a column header.
+ * @return {Array}    The parsed array of data from the CSV string.
+ */
+export const parseCsvString = async (googleCsvString, columnHeaders = false) => (
+  new Promise((resolve, reject) => {
+    const output = []
+    parse(googleCsvString, {
+      columns: columnHeaders,
+    })
+      .on('data', record => output.push(record))
+      .on('error', error => reject(error))
+      .on('end', () => resolve(output))
+  })
+)
