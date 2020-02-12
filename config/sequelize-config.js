3271dfe6b3132ae121dde686a7639bfe24f3fdc6
Dan Schultz <slifty@gmail.com>
2020-02-12 12:43:30 -0500

    Add safety measure to database connections
    We should never allow the test database name to match the production
    database name.  By adding this assertion we ensure that there is almost
    no chance that a data-destructive test be invoked on real data.  This
    overreaches slightly (since database name is just one aspect that could
    make a database unique) but the benefits of being even safer outweigh
    the minor potential inconvenience of having to rename a test database.
    
    Issue #317

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -1,6 +1,31 @@
+const assert = require('assert')
 const dotenv = require('dotenv')
 
 dotenv.config()
+
+// I'm not sure if this should be in an external utility, but given how odd sequelize config
+// is I would like to have this check be directly next to the area where configuration
+// is defined, in case it ever changes.
+//
+// It is CRITICAL that the test database never match produciton / dev, since tests can be
+// destructive to data.
+//
+// I'm wrapping this check in a method name anyway just for the sake of readability.
+const assertSafeDatabaseConfiguration = function() {
+  const testDatabaseURL = new URL(process.env.DATABASE_URL_TEST)
+  const devDatabaseURL = new URL(process.env.DATABASE_URL_DEVELOPMENT)
+  const prodDatabaseURL = new URL(process.env.DATABASE_URL_PRODUCTION)
+  assert(
+    testDatabaseURL.pathname !== devDatabaseURL.pathname,
+    `Test DB (${testDatabaseURL.pathname}) cannot match Development DB (${devDatabaseURL.pathname})`,
+  )
+  assert(
+    testDatabaseURL.pathname !== prodDatabaseURL.pathname,
+    `Test DB (${testDatabaseURL.pathname}) cannot match Production DB (${devDatabaseURL.pathname})`,
+  )
+}
+
+assertSafeDatabaseConfiguration()
 
 module.exports = {
   development: {
